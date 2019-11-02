18f55717fac9f1ff2c399fe69d83402e1050b0ba
acampos <acampos@shipbob.com>
2019-11-01 23:10:13 -0500

    created initial static mapper. have not tested yet but it looks good.

---
+++ SimpleMapper/Attributes/MapTargetAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleMapper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class MapTargetAttribute : Attribute
+    {
+        public Type[] Types { get; private set; }
+        public MapTargetAttribute(params Type[] types)
+        {
+            Types = types;
+            RejectPrimitiveMappings();
+        }
+
+        private void RejectPrimitiveMappings()
+        {
+            if (Types.Any(a => Constants.TypeWhiteList.Contains(a)))
+                throw new ArgumentException($"Only complex types are allowed to be defined as mappable objects");
+        }
+    }
+
+}
